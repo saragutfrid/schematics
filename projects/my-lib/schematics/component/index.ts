56c3ba12d222f4b8662fffb82a0c2122398bc224
saragutfrid <saririenetz@gmail.com>
2024-08-21 15:27:21 +0300

    add

--- projects/my-lib/schematics/component/index.ts
+++ projects/my-lib/schematics/component/index.ts
@@ -1,97 +1,40 @@
-/**
- * @license
- * Copyright Google LLC All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.dev/license
- */
-
 import {
-  FileOperator,
-  Rule,
-  SchematicsException,
-  Tree,
-  apply,
-  applyTemplates,
-  chain,
-  filter,
-  forEach,
-  mergeWith,
-  move,
-  noop,
-  strings,
-  url,
+  Rule, Tree, SchematicContext, chain,
+  externalSchematic, apply, url, template, mergeWith, move
 } from '@angular-devkit/schematics';
-import { addDeclarationToNgModule } from '../utility/add-declaration-to-ng-module';
-import { findModuleFromOptions } from '../utility/find-module';
-import { parseName } from '../utility/parse-name';
-import { validateHtmlSelector } from '../utility/validation';
-import { buildDefaultPath, getWorkspace } from '../utility/workspace';
-import { Schema as ComponentOptions, Style } from './schema';
+import { strings, normalize } from '@angular-devkit/core';
+import { Schema as ComponentOptions } from './schema';
 
-function buildSelector(options: ComponentOptions, projectPrefix: string) {
-  let selector = strings.dasherize(options.name);
-  if (options.prefix) {
-    selector = `${options.prefix}-${selector}`;
-  } else if (options.prefix === undefined && projectPrefix) {
-    selector = `${projectPrefix}-${selector}`;
-  }
-
-  return selector;
-}
-
-export default function (options: ComponentOptions): Rule {
-  return async (host: Tree) => {
-    const workspace = await getWorkspace(host);
-    const project = workspace.projects.get(options.project);
-
-    if (!project) {
-      throw new SchematicsException(`Project "${options.project}" does not exist.`);
+export function myComponent(_options: ComponentOptions): Rule {
+  return async (tree: Tree, _context: SchematicContext) => {
+    // Example of using the tree to read a file
+    const filePath = '/src/app/app.module.ts';
+    if (tree.exists(filePath)) {
+      const content = tree.read(filePath);
+      if (content) {
+        console.log('File content:', content.toString());
+      }
     }
 
-    if (options.path === undefined) {
-      options.path = buildDefaultPath(project);
-    }
+    // Chain your rules as before
+    const angularComponentRule = externalSchematic('@schematics/angular', 'component', {
+      name: _options.name,
+      path: _options.path,
+      project: _options.project,
+      style: _options.style,
+      skipTests: _options.skipTests,
+      inlineStyle: _options.inlineStyle,
+      inlineTemplate: _options.inlineTemplate,
+    });
 
-    options.module = findModuleFromOptions(host, options);
-
-    const parsedPath = parseName(options.path, options.name);
-    options.name = parsedPath.name;
-    options.path = parsedPath.path;
-    options.selector =
-      options.selector || buildSelector(options, (project && project.prefix) || '');
-
-    validateHtmlSelector(options.selector);
-
-    const skipStyleFile = options.inlineStyle || options.style === Style.None;
-    const templateSource = apply(url('./files'), [
-      options.skipTests ? filter((path) => !path.endsWith('.spec.ts.template')) : noop(),
-      skipStyleFile ? filter((path) => !path.endsWith('.__style__.template')) : noop(),
-      options.inlineTemplate ? filter((path) => !path.endsWith('.html.template')) : noop(),
-      applyTemplates({
+    const customTemplates = apply(url('./files'), [
+      template({
+        ..._options,
         ...strings,
-        'if-flat': (s: string) => (options.flat ? '' : s),
-        ...options,
       }),
-      !options.type
-        ? forEach(((file) => {
-            return file.path.includes('..')
-              ? {
-                  content: file.content,
-                  path: file.path.replace('..', '.'),
-                }
-              : file;
-          }) as FileOperator)
-        : noop(),
-      move(parsedPath.path),
+      move(normalize(_options.path as string)),
     ]);
 
-    return chain([
-      addDeclarationToNgModule({
-        type: 'component',
-        ...options,
-      }),
-      mergeWith(templateSource),
-    ]);
+    return chain([angularComponentRule, mergeWith(customTemplates)]);
   };
 }
